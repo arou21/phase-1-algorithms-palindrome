6880d01896e9d5edf3a1fc082661fb1022b33a28
Samuel <smarouba2@gmail.com>
2022-09-28 12:22:12 -0400

    commit tree 58598b75c4d85cdc0469d79d1081586e4d5a30cc

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverseWord = word.split('').reverse().join('')
+  return reverseWord === word 
 }
 
 /* 
   Add your pseudocode here
+  reverse word = word.reverse
+  if word = reverseword
+  then true
 */
 
 /*
   Add written explanation of your solution here
+  isPalimndrom is a function that takes one parmitor that is a string. 
+  when a word comes in we reverse the word by calling the split function, 
+  which converts the string into an array, so that we could reverse the array 
+  using the reverse method. Once the string is reversed, then we convert the 
+  array into a string, and then we check that the word is the same.   
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +30,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log('')
+  console.log('Expecting: true')
+  console.log('=>', isPalindrome('dad'))
 }
 
 module.exports = isPalindrome;
